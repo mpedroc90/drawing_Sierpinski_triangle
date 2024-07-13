006d9ffdfc82a3fec2ff8be65912b311312fa16d
mpedroc90 <mpedroc90@gmail.com>
2024-07-13 13:06:41 +0200

    add firsts draft of media controls

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
-async function* getNextPoint(hash) {
+async function* getNextPoint(hash, stop = () => false, isPause = () => false) {
   let currentPoint = [234, 243];
-  for (let i = 0; i < 10000000; i++) {
+  while (!stop()) {
     const vertex = Math.floor(Math.random() * 3);
 
     const nextPoint = [
@@ -8,8 +8,11 @@ async function* getNextPoint(hash) {
       (hash[vertex][1] + currentPoint[1]) / 2,
     ];
 
-    yield await nextPoint;
+    if (!isPause()) {
+      yield await nextPoint;
+    }
+
     currentPoint = nextPoint;
-    await new Promise((r) => this.setTimeout(r, 10));
+    await new Promise((r) => this.setTimeout(r, 0));
   }
 }
